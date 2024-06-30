7a108fc76ae12c98ac19f5f6bbf036f82c007fd9
Thohir Masruri <ahmdsyukronilhami@gmail.com>
2024-06-30 12:41:08 +0700

    first commit

---
+++ database/migrations/2024_06_13_004458_create_program_studis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('program_studis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('kode_prodi');
+            $table->text('nama_prodi');
+            $table->text('kode_fakultas')->nullable(); // Membuat kolom dapat bernilai null
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program_studis');
+    }
+};
